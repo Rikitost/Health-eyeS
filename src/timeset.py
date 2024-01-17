5a4c0114dffb31ef23bb9a2f5ff8abc3f82cb676
shoki-st1 <kd1303124@st.kobedenshi.ac.jp>
2024-01-17 17:24:16 +0900

    [add]入力された値の判定

---
+++ src/timeset.py
@@ -0,0 +1,25 @@
+# tkinterのimport
+import tkinter as tk
+# グローバル変数をセット
+import time_limit_value as glimit
+
+
+# 入力内容確認の関数
+def value_check(entry_text, warning_label):
+    # 数字の判定
+    if entry_text.get().isdigit() or entry_text.get() == "":
+        return True
+    else:
+        warning_label.config(text='数字を入力してください')
+        return
+# 数値のみ
+
+
+def on_validate_time(d, i, P, s, S, v, V, W):
+    # Pが数字の場合はTrue、それ以外はFalse
+    return (P.isdigit() and len(P) <= 4) or P == ""
+
+
+def on_validate_pass(d, i, P, s, S, v, V, W):
+    # Pが数字の場合はTrue、それ以外はFalse
+    return (P.isdigit() and len(P) <= 4) or P == ""
