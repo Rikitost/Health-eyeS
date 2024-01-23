7bff9ce4872125d5f0ef1bed99fe2fe2a106d2a2
shoki-st1 <kd1303124@st.kobedenshi.ac.jp>
2024-01-23 13:32:21 +0900

    [ract]アイコンを利用

--- src/setup.py
+++ src/setup.py
@@ -13,6 +13,9 @@ elif sys.platform == "darwin":
 else:
     base = None
 
+
+icon_path = "favicon.ico"
+
 build_exe_options = {
     "packages": ["data"],  # 依存するパッケージを指定
     "include_files": ["data/", "clock_thread_end_flg.py", "end_flg_value.py", "form_lock_flg.py", "limit.txt", "pass_sec_value.py", "password_input.py", "password.txt", "restart_flg.py", "setting_thread_end_flg.py", "setting.py", "tempCodeRunnerFile.py", "time_count_flg.py", "time_count_value.py", "timeset.py"],  # 参照するフォルダやリソースを指定
@@ -48,10 +51,10 @@ bdist_msi_options = {
 
 setup(
     name="Health-eyeS",
-    version="2.0.2",
+    version="2.0.3",
     description="Health-eyeS",
     options={"build_exe": build_exe_options, "bdist_msi": bdist_msi_options},
-    executables=[Executable("HealtheyeS.py",  base=base,
+    executables=[Executable("HealtheyeS.py",  base=base, icon=icon_path,
                             shortcut_name="Health-eyeS",)],
 )
 
