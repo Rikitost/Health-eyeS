9e52d3a95cfcb63b397c58332643bade0d960e89
shoki-st1 <kd1303124@st.kobedenshi.ac.jp>
2024-01-23 14:16:10 +0900

    [rf]終了出来る。

--- src/warning.py
+++ src/warning.py
@@ -15,12 +15,13 @@ def rootwin():
     # formの表示関数
     def toggle_visibility_on():
         # ウィンドウの透明度を設定 (0: 完全透明, 1: 完全不透明)
-        root.attributes("-alpha", 0.97)
+        root.attributes("-alpha", 0.95)
 # 非表示---------------------------------------------------------------------------------------------------------------------
 
     def toggle_visibility_off():
         # ウィンドウの透明度を設定 (0: 完全透明, 1: 完全不透明)
-        root.attributes("-alpha", 0)
+        root.attributes("-alpha", 0.0)
+        root.attributes("-transparent", "white")
     # ----------------------------------------------------------------------------------
 
     # form
@@ -30,12 +31,10 @@ def rootwin():
     # ウィンドウの表示
     root.deiconify()
     # ウィンドウを透明クリック可能にする
-
     root.wm_attributes("-transparentcolor", "white")
     # root.geometry("{0}x{1}+0+0".format(3000, 3000))
     # ウィンドウの初期設定
     # 画面全体
-    # root.attributes("-zoomed", "1")
     root.attributes("-fullscreen", True)
     # タスクバー
     root.overrideredirect(True)
@@ -44,24 +43,31 @@ def rootwin():
     # ウィンドウ移動、サイズ変更の無効
     root.bind("<B1-Motion>", lambda event: "break")
     root.bind("<Configure>", lambda event: "break")
+    toggle_visibility_off()
 
-    # 注意画面の判定
-    if warn.flg == 1:
-        toggle_visibility_on()
-    else:
-        toggle_visibility_off()
+    def warning():
+        # 終わりのフラグ
+        if gend.flg == 1:
+            toggle_visibility_off()
+            root.quit()
+            root.destroy()
+        else:
+            # 注意画面の判定
+            if warn.flg == 1:
+                # 注意画面のon
+                toggle_visibility_on()
+            else:
+                # 注意画面のoff
+                toggle_visibility_off()
+            # 1秒ごとに監視いたします
+            root.after(1000, warning)
+        print(warn.flg)
 
-    # 終わりのフラグ
-    if gend.flg == 1:
-        toggle_visibility_off()
-        root.quit()
-        root.destroy()
-    else:
-        # 1秒ごとに監視
-        root.after(1000, rootwin)
+    root.after(100, warning)
 
     root.mainloop()
 
 
 if __name__ == '__main__':
+    warn.flg = 0
     rootwin()
