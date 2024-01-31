1a72e21049b332ebe703e611c578f6887482c79a
shoki-st1 <kd1303124@st.kobedenshi.ac.jp>
2024-01-31 11:43:53 +0900

    [act]リリース

--- src/setup.py
+++ src/setup.py
@@ -50,7 +50,7 @@ bdist_msi_options = {
 
 setup(
     name="Health-eyeS",
-    version="2.1.5",
+    version="3.0",
     description="Health-eyeS",
     options={"build_exe": build_exe_options, "bdist_msi": bdist_msi_options},
     executables=[Executable("HealtheyeS.py",  base=base,
